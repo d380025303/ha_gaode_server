e717de4f344f3313a1ba48ee5fed4e28c56ea031
daixin <daixin@karrytech.com>
2023-06-04 15:16:45 +0800

    feat: 修复bug, 其它包冲突

--- custom_components/ha_gaode_server/__init__.py
+++ custom_components/ha_gaode_server/__init__.py
@@ -38,10 +38,20 @@ def async_setup(hass, config) -> bool:
 
     ha_gaode_server = config["ha_gaode_server"]
     gaode_server_key = ha_gaode_server.get("gaode_server_key")
+    change_gpslogger_state = ha_gaode_server.get("change_gpslogger_state")
+    if change_gpslogger_state is None:
+        change_gpslogger_state = True
+
     if gaode_server_key is None:
         raise ConfigException("未配置高德地图key")
 
-    gpslogger_instance = DxGpsLogger(hass, gaode_server_key)
+    gpslogger_instance = DxGpsLogger(
+        hass,
+        {
+            "gaode_server_key": gaode_server_key,
+            "change_gpslogger_state": change_gpslogger_state,
+        },
+    )
     zone_instance = DxZone(hass)
     gpslogger_view_instance = DxGpsLoggerView(gpslogger_instance)
     zone_view_instance = DxZoneView(zone_instance, hass)
